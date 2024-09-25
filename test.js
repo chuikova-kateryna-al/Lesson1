c363c0b83cd4f24f0ffdc1acf197ab6b7d7b0f04
chuikova-kateryna-al <chuikova.kateryna.al@gmail.com>
2024-09-25 17:02:28 +0200

    checkProbabilityTheory function

--- test.js
+++ test.js
@@ -1,12 +1,35 @@
-function pow (x,y)
-{
-    result = x;
+var count;
+function checkProbabilityTheory(count){
 
-    for (i = 1; i < y; i++ ){
-        result = result*x;
+        var evennum = 0;
+        var oddnum = 0;
+        
+ 
+    for (i = 1; i <= count; i++ ){
+
+        var num = Math.random()*901+100;
+        var int = Math.trunc(num);
+        console.log(int);
+     
+    if (int % 2 === 0){
+        var evennum = evennum + 1;
+    
+    } else {
+        
+       var oddnum = oddnum + 1; 
     }
-    return result;
+    
+    }
+
+    var perc = (evennum/oddnum)*100;
+    console.log('Кількість згенерованих чисел: ' + count);
+    console.log('Парних чисел: ' + evennum);
+    console.log('Не парних чисел: ' + oddnum);
+    console.log('Відсоткове відношення парних чисел до непарних:' + perc +'%');
+    return res;
+    
 }
 
-var res = pow(12,6);
-console.log(res);
+var res = checkProbabilityTheory(19);
+
+
