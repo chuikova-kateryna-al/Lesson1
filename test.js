14863bca108ce50c8aa2ba8ee24ffdaca76eb53b
chuikova-kateryna-al <chuikova.kateryna.al@gmail.com>
2024-09-30 13:44:04 +0200

    adding HW5

--- test.js
+++ test.js
@@ -1,34 +1,20 @@
-var count;
-function checkProbabilityTheory(count){
-
-        var evennum = 0;
-        var oddnum = 0;
-        
-    for (i = 1; i <= count; i++ ){
-
-        var num = Math.random()*901+100;
-        var int = Math.trunc(num);
-        console.log(int);
-     
-    if (int % 2 === 0){
-        var evennum = evennum + 1;
-    
-    } else {
-        
-       var oddnum = oddnum + 1; 
-    }
-
-    }
-
-    var perc = (evennum/oddnum)*100;
-    console.log('Кількість згенерованих чисел: ' + count);
-    console.log('Парних чисел: ' + evennum);
-    console.log('Не парних чисел: ' + oddnum);
-    console.log('Відсоткове відношення парних чисел до непарних:' + perc +'%');
-    return res;
-    
+var hogwartsStudentProfile = {
+    name: "Draco",
+    surname: "Malfoy",
+    age: "15",
+    dateOfBith: "June 5, 1980",
+    house: "Slytherin",
+    bloodStatus: "Pureblood",
+    wand: "hawthorn wood with a unicorn hair core"
 }
 
-var res = checkProbabilityTheory(19);
+function getStudentProfile (){
+    for (let key in hogwartsStudentProfile){
+        console.log(key + ': ' + hogwartsStudentProfile[key])
+    }
+}
+
+hogwartsStudentProfile.patronus = " cannot cast";
 
 
+getStudentProfile();
