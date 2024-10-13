cd9fa23c61a7c382bc0089de8024c47967342120
chuikova-kateryna-al <chuikova.kateryna.al@gmail.com>
2024-10-13 17:42:10 +0200

    final result of HW 7.1

--- test.js
+++ test.js
@@ -18,10 +18,10 @@ var arr = [
 
 function validEmailsArray(arr){
 var reg = /^[a-z0-9]+[.]?[a-z0-9]*@(gmail.com|yahoo.com)$/i;
+var arrayWithEmails =[];
 
 for (i = 0; i < arr.length; i++) {
 for (const [key, value] of Object.entries(arr[i])){
-    var arrayWithEmails =[];
     if (key === 'email'){
         email = value;
         if (reg.test(email)){
@@ -29,8 +29,10 @@ for (const [key, value] of Object.entries(arr[i])){
             } 
         }  
     }
-    var res = console.log(arrayWithEmails);
+    
     }
+    var res = console.log(arrayWithEmails);
     return res;
 }
 (validEmailsArray(arr));
+
