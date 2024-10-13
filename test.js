9c408b16e0cd5ccb9ccbce49dff345a994628f62
chuikova-kateryna-al <chuikova.kateryna.al@gmail.com>
2024-10-13 17:16:48 +0200

    added HW 7.1

--- test.js
+++ test.js
@@ -1,1 +1,36 @@
-const regex = '/\b[^\WaA]{6,}\b/g';
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru"
+    },
+]
+
+function validEmailsArray(arr){
+var reg = /^[a-z0-9]+[.]?[a-z0-9]*@(gmail.com|yahoo.com)$/i;
+
+for (i = 0; i < arr.length; i++) {
+for (const [key, value] of Object.entries(arr[i])){
+    var arrayWithEmails =[];
+    if (key === 'email'){
+        email = value;
+        if (reg.test(email)){
+        arrayWithEmails.push(email);
+            } 
+        }  
+    }
+    var res = console.log(arrayWithEmails);
+    }
+    return res;
+}
+(validEmailsArray(arr));
